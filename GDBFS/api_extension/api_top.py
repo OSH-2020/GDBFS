487d8a9c3b810d54ff6b0918b91878eb371a6ee4
Huang Zhiyhuan <hzy_ustc@mail.ustc.edu.cn>
2020-06-20 17:57:36 +0000

    you can specify a  filename_extension when call api top

--- GDBFS/api_extension/api_top.py
+++ GDBFS/api_extension/api_top.py
@@ -6,9 +6,12 @@ from .api_functions.photo_extraction import keyword_photo_extract
 
 # TODO:增加关键词判优函数，增加API数量，通过不同API返回关键词的比较和综合，得到最终关键词列表
 # TODO:增加音乐、视频等的识别
-def get_keywords_properties(filepath, keys_limit=-1):
+def get_keywords_properties(filepath, keys_limit=-1, filename_extension_specified=None):
     # 获取文件信息
-    filename_extension = filepath.split('.')[-1].lower()
+    if filename_extension_specified is None:
+        filename_extension = filepath.split('.')[-1].lower()
+    else:
+        filename_extension = filename_extension_specified
     file_information = get_file_information(filepath)
     # print(filename_extension)
     if (filename_extension == 'txt') or (filename_extension == 'docx'):
